0f1f948c98a10ae64a35131f4c20c023a4684df6
Max Lapshin <max@maxidoors.ru>
2010-02-22 22:40:55 +0300

    Fixed videoreader plugin start

--- src/videoreader_sup.erl
+++ src/videoreader_sup.erl
@@ -19,19 +19,19 @@ start_link() ->
 
 
 start_reader(Host, Name, Path) ->
-  supervisor:start_child(videoreader_sup, [Host, Name, Path]).
+  supervisor:start_child(video_reader_sup, [Host, Name, Path]).
 
-init([videoreader]) ->
+init([video_reader]) ->
     {ok,
         {{simple_one_for_one, 3, 10},
             [
               % MediaEntry
               {   undefined,                               % Id       = internal id
-                  {videoreader,start_link,[]},             % StartFun = {M, F, A}
+                  {video_reader,start_link,[]},             % StartFun = {M, F, A}
                   temporary,                               % Restart  = permanent | transient | temporary
                   2000,                                    % Shutdown = brutal_kill | int() >= 0 | infinity
                   worker,                                  % Type     = worker | supervisor
-                  [videoreader]                            % Modules  = [Module] | dynamic
+                  [video_reader]                            % Modules  = [Module] | dynamic
               }
             ]
         }
@@ -39,8 +39,8 @@ init([videoreader]) ->
 
 init([]) ->
   Supervisors = [
-    {   videoreader_sup,
-        {supervisor,start_link,[{local, videoreader_sup}, ?MODULE, [videoreader]]},
+    {   video_reader_sup,
+        {supervisor,start_link,[{local, video_reader_sup}, ?MODULE, [video_reader]]},
         permanent,                               % Restart  = permanent | transient | temporary
         infinity,                                % Shutdown = brutal_kill | int() >= 0 | infinity
         supervisor,                              % Type     = worker | supervisor
